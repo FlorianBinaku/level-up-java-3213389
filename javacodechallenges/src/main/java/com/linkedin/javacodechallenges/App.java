f29b8161c5ed1e8ea6664634bae43e1d61642c3a
FlorianBinaku <38474265+FlorianBinaku@users.noreply.github.com>
2023-04-18 11:13:07 +0000

    00

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person florian = new Person("Florian","Binaku");
+
+        florian.setAge(23);
+
+        florian.introduction();
+
+        Person p2 = new Person("Fis","Geci");
+        p2.setAge(22);
+        p2.introduction();
+
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,26 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
+  private String firstName;
+  private String lastName;
   
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+
+  private int age;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void introduction(){
+    System.out.println(String.format("Hello! My name is %s %s and I am %d years old.",firstName,lastName,age));
+  }
 }
